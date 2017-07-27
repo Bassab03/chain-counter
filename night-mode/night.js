cc521429b4a77eb19f59ac5c831484db7e0ab953
Bassab03 <21043977+Bassab03@users.noreply.github.com>
2017-07-27 22:38:38 +0200

    Update night.js

--- night-mode/night.js
+++ night-mode/night.js
@@ -2,7 +2,8 @@ function toggleMode() {
   var toggleButton = document.querySelector('#toggle-button')
   if (toggleButton.innerText == "Night mode") {
     toggleButton.innerText = "Day mode"
-    document.body.style.backgroundColor = "#000000"
+    //document.body.style.backgroundColor = "#000000"
+    document.body.style = "night center-night"
   } else {
     toggleButton.innerText = "Night mode"
     document.body.style.backgroundColor = "#FFFFFF"
