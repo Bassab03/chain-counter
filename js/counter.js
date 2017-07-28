78d751f8a3fcf021113a880f9037f660d889a245
Bassab03 <21043977+Bassab03@users.noreply.github.com>
2017-07-28 15:02:14 +0200

    Update counter.js

--- js/counter.js
+++ js/counter.js
@@ -56,7 +56,7 @@ function getMessageFor(counterValue) {
   if (counterValue === max || counterValue === min) {
     return "LIMIT reached!"
   } else if (counterValue >= best) {
-    return "4 perfect IVs, 15% HA chance, 1/1024 shiny chance"
+    return "4 guaranteed IVs, 15% HA chance, 1/1024 shiny chance"
   } else if (counterValue < none) {
     return "No guaranteed IVs, 0% HA Chance, 1/4096 shiny chance"
   } else if (counterValue >= 0 && counterValue < 5) {
